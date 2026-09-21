8bd465a6a40e0e14fcf3fc0d100378235bc4b83f
kd040 <kdivyam040@gmail.com>
2026-09-21 15:02:47 +0530

    valid Anagarm Hashmap

---
+++ 242_valid anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hash_map = {}
+        for char in s:
+            if char in hash_map:
+                hash_map[char] += 1
+            else:
+                hash_map[char] = 1
+        for char in t:
+            if char in hash_map:
+                hash_map[char] -= 1
+            else:
+                return False
+        for count in hash_map.values():
+            if count != 0:
+                return False
+        return True
